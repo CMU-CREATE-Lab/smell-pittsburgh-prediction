ec3f31d848d4121d59dccce7c86a8df3cf47f174
Yen-Chia Hsu <hsu.yenchia@gmail.com>
2018-10-04 16:19:52 -0400

    Added code for evaluating results.

--- py/prediction/evaluate.py
+++ py/prediction/evaluate.py
@@ -7,31 +7,12 @@ def readInfo(p):
         data = f.readlines()
     if "time series" in data[-1]:
         tail = data[-13]
+    if "residual" in data[-1]:
+        tail = data[-7]
     else:
         tail = data[-12]
     tail = tail.strip().replace("'", "\"")
     return pd.read_json("[" + tail + "]", orient="records")
-
-def main1():
-    path = "data_main/log/classification/result/"
-    cols = ["TP","FP","FN","precision","recall","fscore"]
-    et = []
-    rf = []
-    for name in getAllFileNamesInFolder(path):
-        if "ET" in name:
-            et.append(readInfo(path + name))
-        elif "RF" in name:
-            rf.append(readInfo(path + name))
-    df_et = pd.concat(et) # ExtraTrees
-    df_rf = pd.concat(rf) # Random Forest
-    print df_et
-    print df_rf
-    print "----------------"
-    print "ExtraTrees"
-    print df_et.describe()
-    print "----------------"
-    print "Random Forest"
-    print df_rf.describe()
 
 def main2():
     path = "data_main/analysis/log/result/"
@@ -46,5 +27,36 @@ def main2():
     print "Decision Tree"
     print df_dt.describe()
 
-main1()
-main2()
+def evaluate(path, rule):
+    cols = ["TP","FP","FN","precision","recall","fscore"]
+    d = []
+    for name in getAllFileNamesInFolder(path):
+        if rule in name:
+            d.append(readInfo(path + name))
+    df = pd.concat(d) # Decision Tree
+    print df
+    print df.describe()
+
+def main():
+    print "------------------------------------------------------"
+    print "------------------------------------------------------"
+    print "Classification ExtraTrees"
+    evaluate("data_main/log/classification/result/", "ET")
+    print "------------------------------------------------------"
+    print "------------------------------------------------------"
+    print "Classification Random Forest"
+    evaluate("data_main/log/classification/result/", "RF")
+    print "------------------------------------------------------"
+    print "------------------------------------------------------"
+    print "Regression ExtraTrees"
+    evaluate("data_main/log/regression/result/", "ET")
+    print "------------------------------------------------------"
+    print "------------------------------------------------------"
+    print "Regresssion Random Forest"
+    evaluate("data_main/log/regression/result/", "RF")
+    print "------------------------------------------------------"
+    print "------------------------------------------------------"
+    print "Decision Tree"
+    evaluate("data_main/analysis/log/result/", "DT")
+
+main()
