dae7efcffb67cb273a9acf501e4e70768a1747d6
Yen-Chia Hsu <hsu.yenchia@gmail.com>
2018-12-20 18:14:30 -0500

    Added conda environment config.

--- install_packages.sh
+++
@@ -1,5 +0,0 @@
-# For Mac and Linux
-while read requirement; do conda install --yes $requirement; done < requirements.txt
-
-# For Windows
-#FOR /F "delims=~" %f in (requirements.txt) DO conda install --yes "%f" || pip install "%f"
