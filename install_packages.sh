4a82b5a21e5d4386f22bc607c988d99261e4843c
Yen-Chia Hsu <hsu.yenchia@gmail.com>
2019-01-07 17:13:40 -0500

    Added a list of required packages.

---
+++ install_packages.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+pip install torch torchvision
+pip install requests
+pip install pytz
+pip install pandas
+pip install sklearn
+pip install imblearn
+pip install matplotlib
+pip install nltk
+pip install seaborn
+pip install google-api-python-client
+pip install oauth2client
+pip install joblib
+pip install statsmodels
